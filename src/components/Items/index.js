6eac9779f73bb5fc661d1308a6a686ca463be4d5
nakayan5 <tomoya.progress@gmail.com>
2020-10-13 03:38:31 +0900

    searchpage-modified

--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -1,3 +1,4 @@
 export {default as Items} from './Items'
 export {default as SideItems} from './SideItems'
 export {default as CartListItem} from './CartListItem'
+export {default as SearchItems} from './SearchItems'
